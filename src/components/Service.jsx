1816dc19c4c7225ef9fb78d45bb733280b11717f
Henry Frizzel Johanis <izzzzzzell2401@gmail.com>
2025-05-29 04:56:14 +0700

    updatee bug scroll7

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -4,9 +4,9 @@ import "../service.css"
 export default function Service({ title, subtitle1, subtitle2 }) {
 
   return (
-    <div className='h-[200dvh] relative bg-[#98A869] z-[1]'>
-      <div className="sticky top-0 h-screen w-full overflow-hidden">
-        <div className='relative z-[2] flex w-full h-full flex-col justify-center items-center px-2'>
+    <div className='relative bg-[#98A869]'>
+      <div className="min-h-[200dvh]">
+        <div className="fixed inset-0 flex flex-col justify-center items-center px-2 pointer-events-none">
           <svg width="100%" className='lg:h-[140px] md:h-[118px] sm:h-[100px] h-[100px]'>
             <defs>
               <filter id="dropshadow">
@@ -29,7 +29,7 @@ export default function Service({ title, subtitle1, subtitle2 }) {
                 <tspan>{title}</tspan>
               </text>
           </svg>
-          <svg width="100%" height={"60"} className='md:h-[60px] h-35px relative z-[2]'>
+          <svg width="100%" height={"60"} className='md:h-[60px] h-35px'>
             <defs>
               <filter id="dropshadow">
                 <feDropShadow dx="2" dy="2.5" stdDeviation="0" flood-color="black" flood-opacity="1" />
@@ -51,7 +51,7 @@ export default function Service({ title, subtitle1, subtitle2 }) {
                 <a className='svg-hover cursor-pointer relative z-99' href='https://linktr.ee/AdvokesmaBEMFIAUB' target='_blank'>{subtitle1}</a>
               </text>
           </svg>
-          <svg width="100%" className='md:h-[60px] h-[35px] relative z-[2]'>
+          <svg width="100%" className='md:h-[60px] h-[35px]'>
             <defs>
               <filter id="dropshadow">
                 <feDropShadow dx="2" dy="2.5" stdDeviation="0" flood-color="black" flood-opacity="1" />
@@ -74,19 +74,23 @@ export default function Service({ title, subtitle1, subtitle2 }) {
               </text>
           </svg>
         </div>
-        <div className="absolute bottom-0 left-0 right-0 w-full z-[1]">
+        
+        <div className="fixed bottom-0 left-0 right-0 w-full">
           <img 
             src={pinkBottom} 
             alt="" 
-            className="w-full object-cover transform-gpu will-change-transform" 
+            className="w-full object-cover transform-gpu" 
             style={{
               backfaceVisibility: 'hidden',
-              WebkitBackfaceVisibility: 'hidden'
+              WebkitBackfaceVisibility: 'hidden',
+              transform: 'translate3d(0,0,0)'
             }}
           />
         </div>
       </div>
-      <div className="h-[100dvh] bg-transparent"></div>
+
+      {/* This ensures proper spacing for the next section */}
+      <div className="h-screen" />
     </div>
   )
 }
